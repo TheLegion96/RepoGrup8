ce58a2d3d5871e337f58e5bde3cce84325f81675
Andrea Tonnini <at.rsm@hotmail.it>
2017-07-11 17:40:28 +0200

    First Pull Commit

---
+++ Gruppo 8/GridMovements/GridMovements/Assets/Scripts/EnemySpecularScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpecularScript : MonoBehaviour {
+    bool Moving = false;
+    private int inputPlayer = 0;
+    private Vector3 pos;
+    public float Step = 0.1f;
+    public float speed =5f;
+   public  GameObject[] allPrefab;
+    public int Counter=0;
+
+    // Use this for initialization
+    void Start()
+    {
+        pos = transform.position;
+        allPrefab = Resources.LoadAll<GameObject>("Prefab");
+        Debug.Log(allPrefab.Length);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+   
+    }
+
+public void CallSpecularMovement(int _inputPlayer)
+    {
+        inputPlayer = _inputPlayer;
+        pos = transform.position;
+        switch (inputPlayer)
+        {
+            case 1:
+                pos += Vector3.left;
+         
+                break;
+            case 2:
+                pos += Vector3.right;
+      
+                break;
+            case 3:
+                pos += Vector3.up;
+            
+                break;
+            case 4:
+                pos += Vector3.down;
+          
+                break;
+            default:
+                inputPlayer = 0;
+             
+                break;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime*speed);
+        inputPlayer = 0;
+        Moving = false;
+    }
+    public int CounterUntilFlip=0;
+
+    public void CallABMovement()
+    {
+       
+        bool direction = true;
+        pos = transform.position;
+        if (direction)
+        {
+            pos += Vector3.left;
+            CounterUntilFlip++;
+        }
+        else
+        {
+            pos += Vector3.right;
+            CounterUntilFlip++;
+        }
+        if(CounterUntilFlip<=5)
+        {
+            if (direction)
+                direction = false;
+            else
+                direction = true;
+
+        }
+    }
+
+}
---
+++ Gruppo 8/GridMovements/GridMovements/Assets/Scripts/GridMovement.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DIRECTION { UP, DOWN, LEFT, RIGHT}
+
+public class GridMovement : MonoBehaviour {
+
+    private bool canMove = true;
+    private bool moving = false;
+    private float speed = 5f;
+    private float buttonCooldown = 0f;
+    private DIRECTION dir = DIRECTION.DOWN;
+    private Vector3 pos;
+    private int inputPlayer = 0;
+    public EnemySpecularScript[] en;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        buttonCooldown--;
+
+        if (canMove)
+        {
+            pos = transform.position;
+            move();
+        }
+
+        if (moving)
+        {
+            if(transform.position == pos)
+            {
+                moving = false;
+                canMove = true;
+
+                move();
+             
+            }
+
+            transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+            foreach (EnemySpecularScript em in en)
+            {
+                em.CallSpecularMovement(inputPlayer);
+                em.CallABMovement();
+            }
+        }
+		
+	}
+
+    private void move()
+    {
+        if(buttonCooldown <= 0)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (dir != DIRECTION.UP)
+                {
+                    //buttonCooldown = 0.3f;
+                    inputPlayer = 4;
+                    dir = DIRECTION.UP;
+                }
+                else
+                {
+                    canMove = false;
+                    moving = true;
+                    pos += Vector3.up;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                if (dir != DIRECTION.DOWN)
+                {
+                    inputPlayer = 3;
+                    //buttonCooldown = 0.3f;
+                    dir = DIRECTION.DOWN;
+                }
+                else
+                {
+                    canMove = false;
+                    moving = true;
+                    pos += Vector3.down;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                if(dir != DIRECTION.LEFT)
+                {
+                    inputPlayer = 2;
+                    //buttonCooldown = 0.3f;
+                    dir = DIRECTION.LEFT;
+                }
+                else
+                {
+                    canMove = false;
+                    moving = true;
+                    pos += Vector3.left;
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                if (dir != DIRECTION.RIGHT)
+                {
+                    inputPlayer = 1;
+                    //buttonCooldown = 0.3f;
+                    dir = DIRECTION.RIGHT;
+                }
+                else
+                {
+                    canMove = false;
+                    moving = true;
+                    pos += Vector3.right;
+                }
+
+            }
+
+        }
+    }
+
+}
