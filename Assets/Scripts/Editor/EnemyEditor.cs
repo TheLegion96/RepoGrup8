2be145716432fbd705ad18f1895001023defa4d4
CabbageWarrior <feelthecabbage@gmail.com>
2017-07-17 16:53:41 +0200

    Added elements position helper. Removed useless walls.

---
+++ Assets/Scripts/Editor/EnemyEditor.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using Completed;
+
+[CustomEditor(typeof(EnvironmentObject))]
+public class EnvironmentObjectEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        EnvironmentObject myObj = (EnvironmentObject)target;
+
+        GUILayout.BeginHorizontal();
+        GUILayout.BeginVertical();
+        GUILayout.EndVertical();
+        GUILayout.BeginVertical();
+        if (GUILayout.Button("Move Up"))
+        {
+            myObj.Move(Vector2.up);
+        }
+        GUILayout.EndVertical();
+        GUILayout.BeginVertical();
+        GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.BeginVertical();
+        if (GUILayout.Button("Move Left"))
+        {
+            myObj.Move(Vector2.left);
+        }
+        GUILayout.EndVertical();
+        GUILayout.BeginVertical();
+        GUILayout.EndVertical();
+        GUILayout.BeginVertical();
+        GUILayout.EndVertical();
+        if (GUILayout.Button("Move Right"))
+        {
+            myObj.Move(Vector2.right);
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal();
+        GUILayout.BeginVertical();
+        GUILayout.EndVertical();
+        GUILayout.BeginVertical();
+        if (GUILayout.Button("Move Down"))
+        {
+            myObj.Move(Vector2.down);
+        }
+        GUILayout.EndVertical();
+        GUILayout.BeginVertical();
+        GUILayout.EndVertical();
+        GUILayout.EndHorizontal();
+
+    }
+}
---
+++ Assets/Scripts/EnvironmentObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Completed
+{
+	public class EnvironmentObject : MonoBehaviour
+	{
+		public void Move(Vector2 direction) {
+            transform.position += (Vector3)direction;
+        }
+	}
+}
