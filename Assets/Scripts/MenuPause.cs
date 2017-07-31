86e74ed91e8a893bc65f6d8c22bac6501047e536
CabbageWarrior <feelthecabbage@gmail.com>
2017-07-31 11:49:22 +0200

    Pause menu working.

--- Assets/Scripts/MenuPause.cs
+++ Assets/Scripts/MenuPause.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
 
 public class MenuPause : MonoBehaviour
 {
@@ -17,6 +19,7 @@ public class MenuPause : MonoBehaviour
     [Header("MenuVoices")]
     public GameObject[] MenuVoices;
     public GameObject MenuPointer;
+    private int menuIndex;
 
     // Use this for initialization
     void Start()
@@ -31,45 +34,87 @@ public class MenuPause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Open/Close Pause menu.
         if (GameManager.instance.state == GameManager.State.Play || GameManager.instance.state == GameManager.State.Pause)
         {
             if (Input.GetKeyDown(KeyCode.P) && player.isStillAlive)
             {
                 if (transform.position == pause[pauseIndex].position)
                 {
-                    //bookClosedMenuSubtitleMeshRenderer.enabled = false;
-                    pauseIndex++;
-                    GetComponent<AudioSource>().Play();
-                    if (GameManager.instance != null) GameManager.instance.state = GameManager.State.Pause;
+                    OpenMenu();
                 }
             }
             if (Input.GetKeyDown(KeyCode.P) && player.isStillAlive)
             {
                 if (pauseIndex >= pause.Length)
                 {
-                    //bookClosedMenuSubtitleMeshRenderer.enabled = true;
-                    pauseIndex = 0;
-                    if (GameManager.instance != null) GameManager.instance.state = GameManager.State.Play;
+                    CloseMenu();
                 }
 
             }
         }
 
-        //if (transform.position == pause[1].position)
-        //{
-        //    Time.timeScale = 0;
-        //}
+        //Move into Pause menu.
+        if (GameManager.instance.state == GameManager.State.Pause && (
+                Input.GetKeyDown(KeyCode.UpArrow) ||
+                Input.GetKeyDown(KeyCode.DownArrow)
+            ))
+        {
+            menuIndex = Mathf.Clamp(menuIndex - (int)Input.GetAxisRaw("Vertical"), 0, MenuVoices.Length - 1);
+            SetMenuVoice(menuIndex);
+        }
 
         if (transform.position != pause[pauseIndex].position)
-        { 
+        {
             transform.position = Vector3.MoveTowards(transform.position, pause[pauseIndex].position, bookSpeed * Time.deltaTime); // Move the object to Arrays
         }
         if (transform.position == pause[0].position)
         {
             bookClosedMenuSubtitleMeshRenderer.enabled = true;
+            menuIndex = 0;
+            SetMenuVoice(menuIndex);
         }
-        else {
+        else
+        {
+            bookClosedMenuSubtitleMeshRenderer.enabled = false;
+        }
+
+        //Confirm selection into Pause menu.
+        if (GameManager.instance.state == GameManager.State.Pause && Input.GetKeyDown(KeyCode.Return))
+        {
+            switch (MenuVoices[menuIndex].name)
+            {
+                case "MenuVoiceBestiario":
+
+                    break;
+                case "MenuVoiceRiprendi":
+                    CloseMenu();
+                    break;
+                case "MenuVoiceRicominciaStanza":
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+                    break;
+                case "MenuVoiceRicominciaLivello":
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+                    break;
+                case "MenuVoiceTornaAlMainMenu":
+                    SceneManager.LoadScene("MainMenu_simone", LoadSceneMode.Single);
+                    break;
+            }
+        }
+
+        //Move book from A to B.
+        if (transform.position != pause[pauseIndex].position)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, pause[pauseIndex].position, bookSpeed * Time.deltaTime); // Move the object to Arrays
+        }
+        if (transform.position == pause[0].position)
+        {
+            bookClosedMenuSubtitleMeshRenderer.enabled = true;
+            menuIndex = 0;
+            SetMenuVoice(menuIndex);
+        }
+        else
+        {
             bookClosedMenuSubtitleMeshRenderer.enabled = false;
         }
 
@@ -89,5 +134,43 @@ public class MenuPause : MonoBehaviour
             transform.position = new Vector3(0f, 10f, 0);
             isPaused = false;*/
 
+    }
+
+    private void OpenMenu()
+    {
+        //bookClosedMenuSubtitleMeshRenderer.enabled = false;
+        pauseIndex++;
+        GetComponent<AudioSource>().Play();
+        if (GameManager.instance != null) GameManager.instance.state = GameManager.State.Pause;
+    }
+
+    private void CloseMenu()
+    {
+        //bookClosedMenuSubtitleMeshRenderer.enabled = true;
+        pauseIndex = 0;
+        if (GameManager.instance != null) GameManager.instance.state = GameManager.State.Play;
+    }
+
+    private void SetMenuVoice(int menuIndex)
+    {
+        GameObject tempItem;
+        Vector3 defaultLocalScale = new Vector3(1, 1, 1);
+        Vector3 selectedLocalScale = new Vector3(1.1f, 1.1f, 1);
+
+        for (int i = 0; i < MenuVoices.Length; i++)
+        {
+            tempItem = MenuVoices[i];
+
+            if (i == menuIndex)
+            {
+                tempItem.transform.localScale = selectedLocalScale;
+            }
+            else if (tempItem.transform.localScale == selectedLocalScale)
+            {
+                tempItem.transform.localScale = defaultLocalScale;
+            }
+        }
+
+        MenuPointer.transform.position = new Vector3(MenuPointer.transform.position.x, MenuVoices[menuIndex].transform.position.y, MenuPointer.transform.position.z);
     }
 }
