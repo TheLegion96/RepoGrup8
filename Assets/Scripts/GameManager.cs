81b1faae1da5d2023cd121af3f1c25805744419e
CabbageWarrior <feelthecabbage@gmail.com>
2017-08-03 10:44:00 +0200

    Added method to set token kept on finish level and correct steps.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -42,6 +42,7 @@ namespace Completed
         private string title;
         private string subtitle;
         private string chapterText;
+        private int maxStepsForToken;
         public static bool setRestartAvailable = false;
 
         public string Title
@@ -102,6 +103,19 @@ namespace Completed
                     }
                     UpdateSceneBookProperty(bookChapterTextTextMesh, tmpChapterTextAccaped);
                 }
+            }
+        }
+
+        public int MaxStepsForToken
+        {
+            get
+            {
+                return maxStepsForToken;
+            }
+
+            set
+            {
+                maxStepsForToken = value;
             }
         }
 
@@ -404,22 +418,24 @@ namespace Completed
 
         //GameOver is called when the player reaches 0 food points
         public void GameOver()
-        {    setRestartAvailable = true;
+        {
+            setRestartAvailable = true;
             //Set levelText to display number of levels passed and game over message
             levelTitleText.text = "Sei morto nel livello " + level + "!\n Premi R per ricominciare il quadro";
 
             //Enable black background image gameObject.
             levelImage.SetActive(true);
 
-        
+
             //Disable this GameManager.
             // enabled = false;
         }
 
         #region Go To Next Scene
         //Goes to next scene when called.
-        public void GoToNextScene(float delay)
+        public void GoToNextScene(float delay, int playerSteps)
         {
+            SetTokenInfo(playerSteps);
             Invoke("GoToNextScene_Invoked", delay);
         }
         private void GoToNextScene_Invoked()
@@ -431,6 +447,14 @@ namespace Completed
             //Load the last scene loaded, in this case Main, the only scene in the game. And we load it in "Single" mode so it replace the existing one
             //and not load all the scene object in the current scene.
             SceneManager.LoadScene(buildIndex, LoadSceneMode.Single);
+        }
+
+        public void SetTokenInfo(int playerSteps)
+        {
+            if (playerSteps <= maxStepsForToken)
+            {
+                // BRAVO! SETTARE IL TOKEN.
+            }
         }
         #endregion
     }
