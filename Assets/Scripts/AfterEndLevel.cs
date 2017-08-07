6902b298c6c5b3260cd12fcb2934a47b8dfce498
CabbageWarrior <feelthecabbage@gmail.com>
2017-08-08 00:47:38 +0200

    Added correct Return button on final scene.

--- Assets/Scripts/AfterEndLevel.cs
+++ Assets/Scripts/AfterEndLevel.cs
@@ -4,20 +4,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-public class AfterEndLevel : MonoBehaviour {
+public class AfterEndLevel : MonoBehaviour
+{
 
     public TextMesh stepsValueTextMesh;
     public SpriteRenderer endSceneFader;
+    public SpriteRenderer returnToMenuButton;
+
+    private bool isAuthorizedToReturnToMenu = false;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         stepsValueTextMesh.text = stepsValueTextMesh.text.Replace("@", GameManager.instance.playerTotalMoney.ToString());
 
-        endSceneFader.DOFade(0, 3);
+        endSceneFader.DOFade(0, 3).OnComplete(FaderFadeComplete);
     }
-	
-	//// Update is called once per frame
-	//void Update () {
-		
-	//}
+
+    void FaderFadeComplete()
+    {
+        StartCoroutine(EnableReturnButtonCoroutine());
+    }
+
+    IEnumerator EnableReturnButtonCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        returnToMenuButton.DOFade(1, 1);
+        isAuthorizedToReturnToMenu = true;
+        yield return null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (isAuthorizedToReturnToMenu)
+                GameManager.instance.GoToScene(1);
+        }
+    }
 }
